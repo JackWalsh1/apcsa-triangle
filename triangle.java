721e0f8101f792ca4c91dd1ce090c1ac155f69c9
Jack Walsh <jwalsh@ucvts.org>
2019-10-01 08:45:57 -0400

    Test for completion.

---
+++ triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the base length. ");
+        double base = in.nextDouble();
+        in.nextLine();
+
+        System.out.print("Enter the height of the triangle. ");
+        double height = in.nextDouble();
+        in.nextLine();
+
+        double area = (base * height) / 2;
+
+        System.out.println("The area of a triangle with a height and base of " + height + " and " + base + " is " + area + ".");
+
+        in.close();
+    }
+}
